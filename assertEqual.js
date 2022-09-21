68ae76c737bca8d1be97c78ca8686d85cb344c18
Paige Nelmes <designer@paigenelmes.com>
2022-09-21 13:11:50 -0700

    Adding Template Literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 //Function implementation
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log("❌❌❌ Assertion Failed: " + actual + " !== " + expected);
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 //Test code

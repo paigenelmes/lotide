d48e88f2e1d17e1865d0d3caf6876f32c14f50e6
Paige Nelmes <designer@paigenelmes.com>
2022-09-21 12:48:27 -0700

    Adding Assert Equal

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+//Function implementation
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log("❌❌❌ Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+//Test code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Paige", "Paige");
+(assertEqual(3, 4));

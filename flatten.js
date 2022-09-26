9623f81b8e1cde993aca765733912d1699362d11
Paige Nelmes <designer@paigenelmes.com>
2022-09-26 11:07:22 -0700

    Lint fixes

--- flatten.js
+++ flatten.js
@@ -28,4 +28,8 @@ const flatten = function(originalArray) {
   return flatArray;
 };
 
+//Testing
+
 console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]),flatten([1, 2, [3, 4], 5, [6]]));

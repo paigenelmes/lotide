31a6fd8ff4258b1eba9f69166d490ff2a4390453
Paige Nelmes <designer@paigenelmes.com>
2022-10-12 16:00:18 -0700

    Refactoring Middle Test with Mocha & Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,22 +1,28 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
+//const assertArraysEqual = require('../assertArraysEqual');
 
-//1 or 2 elements testing
-
-console.log("Returns empty array when only 1 element:", middle([1])); // => [] (this works!)
-console.log("Returns empty array only 2 elements:", middle([1, 2])); // => [] (this works!)
-
-//Odd number testing
-
-console.log("Middle with odd numbers in array:", middle([1, 2, 3])); // => [2] (this works!)
-console.log("Middle with odd numbers in array:", middle([1, 2, 3, 4, 5])); // => [3] (this works!)
-
-//Even number testing
-
-console.log("Middle with even numbers in array:", middle([1, 2, 3, 4])); // => [2, 3] (this works!)
-console.log("Middle with even numbers in array:", middle([1, 2, 3, 4, 5, 6])); // => [3, 4] (this works!)
-
-//Testing with assertArraysEqual
-
-assertArraysEqual(middle([1, 2, 3, 4, 5]), middle([1, 2, 3, 4, 5])); //passes
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), middle([1, 2, 3, 4, 5, 6])); //passes
+//Testing
+describe("#middle", () => {
+  it("Should return an empty array when given an array with only one element", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("Should return an empty array when given an array with only two elements", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("Shouuld return [2]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("Should return [3]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("Should return [2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("Should return [3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+  it("Should be equal", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), middle([1, 2, 3, 4, 5, 6]));
+  });
+});

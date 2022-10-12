98ec3e9fee0f489ad03f7d5aeae838ccb13519f3
Paige Nelmes <designer@paigenelmes.com>
2022-10-12 15:44:01 -0700

    Refactoring Tail Test with Mocha & Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,14 +1,24 @@
+//Requirements
 const tail = require('../tail');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-
-//Test cases
+//Variables for testing
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertArraysEqual(result, ["Lighthouse", "Labs"]); //Passes; these arrays are a match
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-assertArraysEqual(words.length, 3); // Passes; these lengths are a match
-//Test one element
-const oneElement = tail(["Paige"]);
-console.log("Tail with One Element:", tail(oneElement)); // The array with one element yields an empty array as the tail
-//Test empty array
-console.log("Tail with Empty Array:", tail([])); // Empty array returns an empty array as expected
+
+//Testing
+describe("#tail", () => {
+  it("The tail of the result variable should be equal to the tail of the array", () => {
+    assert.deepEqual(tail(result), tail(["Lighthouse", "Labs"]));
+  });
+  it("An array with one element should be equal to an empty array", () => {
+    assert.deepEqual(tail(["Paige"]), []);
+  });
+  it("An empty array should be equal to an empty array", () => {
+    assert.deepEqual(tail([]), []);
+  });
+  it("The length of the words array should be equal to 3", () => {
+    assert.deepEqual(tail[words.length], tail[3]);
+  });
+
+});
